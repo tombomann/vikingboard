a95295673c70b94b2a557ffb3e98b3687d8d9a0c
tombomann <tomarnejensen@gmail.com>
2025-12-06 00:21:51 +0100

    v4.3.1 structure+PIO

---
+++ firmware/src/rf_sniffer.cpp
@@ -0,0 +1,5 @@
+// firmware/src/rf_sniffer.cpp - Multi-band sniffer (CC1101/SX1262/NRF24)
+#include <Arduino.h>
+
+void setup() { Serial.begin(115200); }
+void loop() { /* TODO: Init radios */ delay(1000); }
